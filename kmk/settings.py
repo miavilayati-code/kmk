7224ba50c347817638957e549ff9b589aa41116a
ananimatul73-star <ananimatul73@gmail.com>
2026-04-04 11:49:51 +0700

    Migrasi database ke MYSQL

--- kmk/settings.py
+++ kmk/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'kmk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'absensi_santri',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
