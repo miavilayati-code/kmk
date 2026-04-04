1b8ab1ebe8d88a95f06d945c3f0e914f930e9dea
ananimatul73-star <ananimatul73@gmail.com>
2026-04-04 12:39:18 +0700

    Setup MYSQL database

--- kmk/settings.py
+++ kmk/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'absensi_santri',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
